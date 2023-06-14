257a367a40624ef5d3c2fea5ac2eef72471aaab5
Azizbek <developerxasanov@gmail.com>
2023-06-14 14:07:19 +0300

    add new p teg

--- app.js
+++
@@ -1,9 +0,0 @@
-const progress = document.querySelector('#progress');
-
-window.addEventListener("scroll", () => {
-    let height = document.body.scrollHeight - window.innerHeight;
-    let positionScroll = document.documentElement.scrollTop;
-    let width = (positionScroll / height ) * 100;
-    progress.style.width = `${width}%`;
-})
-
